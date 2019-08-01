fd7c3f669a196b8a151e209b64b9d60636253b58
reesbritt <reesbritt@hotmail.co.uk>
2019-08-01 16:37:13 +0100

    Added basic login page, no login functionality as of yet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
-import RegistrationForm from "./Components/RegistrationForm.jsx";
+// import RegistrationForm from "./Components/RegistrationForm.jsx";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <LoginForm />
     </div>
   );
 }
