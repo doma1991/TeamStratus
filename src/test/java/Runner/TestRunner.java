fcab9b5e4dda0819e103529e21c3688541b0dc92
omd02 <omarmateus40@gmail.com>
2019-08-01 20:24:27 +0100

    able to generate a report for the tests

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,12 +1,13 @@
 package Runner;
 
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/features"},glue = "Steps")
-public class TestRunner {
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/features"},plugin = { "json:target/cucumber.json", "html:target/site/cucumber-pretty"},glue = "Steps",monochrome = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
