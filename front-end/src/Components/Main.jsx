757d7918b4cfc41b5ff463e6a5372f4d5ff2cd30
reesbritt <reesbritt@hotmail.co.uk>
2019-08-02 15:08:31 +0100

    Changed main react method to display the search page on the root page once again and a number of components were re-added - RB

--- front-end/src/Components/Main.jsx
+++ front-end/src/Components/Main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import RegistrationForm from "./RegistrationForm.jsx";
+// import RegistrationForm from "./RegistrationForm.jsx";
 // import LoginForm from "./LoginForm";
 // import { Switch, Route } from "react-router-dom";
-// import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar";
 
 class Main extends React.Component {
   render() {
     return (
       <main>
-        <RegistrationForm />
+        <SearchBar />
       </main>
     );
   }
