ff0fa242cebfa4a13e76eee9b6c88c0be4143a18
reesbritt <reesbritt@hotmail.co.uk>
2019-08-04 22:54:49 +0100

    Overhauled the aesthetic of both the navbar and the login page, included sky stylesheets for general page layout and also for the login page - RB

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,23 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-import  Navbar  from './Components/Navbar.jsx';
-import  Main  from './Components/Main';
-import  RegistrationForm  from './Components/RegistrationForm.jsx';
-import  LoginForm  from "./Components/LoginForm";
+import React from "react";
+import "./App.css";
+import "./toolkit-core.min.css";
+import "./login.css";
+import Navbar from "./Components/Navbar.jsx";
+import Main from "./Components/Main";
+import RegistrationForm from "./Components/RegistrationForm.jsx";
+import LoginForm from "./Components/LoginForm";
 import { Router } from "@reach/router";
 import "react-datepicker/dist/react-datepicker.css";
 import { DatePicker } from "react-datepicker";
-
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Router>
-        <Main path='/' />
-        <RegistrationForm path='RegistrationForm' />
-        <LoginForm path='LoginForm' /> 
+        <Main path="/" />
+        <RegistrationForm path="RegistrationForm" />
+        <LoginForm path="LoginForm" />
       </Router>
     </div>
   );
--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Link } from "@reach/router";
-
-
+import React from "react";
 const Navbar = () => {
-                 return(
-                    <nav className="navbar navbar-dark bg-dark">
-                        <span className="navbar-brand mb-0 h1"><Link to="/">GetYourWay</Link></span>
+  return (
+    <nav className="navbar d-flex flex-row justify-content-between navbar-dark">
+      <div className="mob-logo">
+        <a
+          href="https://www.sky.com"
+          className="mast-c-header__logo mast-js-focus"
+          data-tracking-label="masthead_home_logo"
+        >
+          <i className="fas fa-bars" />
+        </a>
 
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="login">
-                                                <Link to="LoginForm"> Login </Link></button>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="register">
-                        <Link to="RegistrationForm"> Register </Link></button>
-                        
-                    </nav>
-                 )};
+        <span className="navbar-brand mb-0 h1">GetYourWay</span>
+      </div>
+      <div className="main-logo">
+        <a
+          href="https://www.sky.com"
+          className="mast-c-header__logo mast-js-focus"
+          data-tracking-label="masthead_home_logo"
+        />
 
+        <span className="navbar-brand mb-0 h1">GetYourWay</span>
+      </div>
+
+      <div>
+        <button
+          className="btn btn-outline-primary my-2 my-sm-0"
+          type="register"
+        >
+          Register
+        </button>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
