b584a912b40739c41b3a7b7d16543c7d8547174f
reesbritt <reesbritt@hotmail.co.uk>
2019-08-07 15:20:50 +0100

    Added skycons support and fixed the display - RB

--- front-end/src/Components/PopularWeather.jsx
+++ front-end/src/Components/PopularWeather.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./weather.css";
-// const Skycons= require("skycons")(window);
+const Skycons = require("skycons")(window);
 
-// var skycons = new Skycons({"color" : "blue"});
+var skycons = new Skycons({ color: "blue" });
 
 class PopularWeather extends React.Component {
   constructor(props) {
     super(props);
+    this.ref1 = React.createRef();
+    this.ref2 = React.createRef();
+    this.ref3 = React.createRef();
+    this.ref4 = React.createRef();
+    this.ref5 = React.createRef();
+
     this.state = {
       weather1: "",
       weather2: "",
@@ -28,6 +34,12 @@ class PopularWeather extends React.Component {
       currency3: "",
       currency4: "",
       currency5: ""
+      // icon1: "CLEAR_NIGHT",
+      // icon2: "",
+      // icon3: "",
+      // icon4: "",
+      // icon5: "",
+      // icon: []
     };
 
     this.getTopFiveWeather("weather1", "40.7128", "74.0060", "temp1");
@@ -77,7 +89,48 @@ class PopularWeather extends React.Component {
         this.setState({
           [value2]: responseJson.currently.temperature
         });
-        // skycons.add("icon1", skycons.PARTYLY_CLOUDY_DAY);
+
+        skycons.add("icon1", skycons.responseJson.currently.icon);
+        // skycons.add("icon2", skycons.responseJson.currently.icon);
+        // skycons.add("icon3", skycons.responseJson.currently.icon);
+        // skycons.add("icon4", skycons.responseJson.currently.icon);
+        // skycons.add("icon5", skycons.responseJson.currently.icon);
+
+        console.log(skycons.responseJson.currently.icon);
+
+        if (!skycons.responseJson.currently.icon) {
+          return <h2>Loading...</h2>;
+        }
+
+        if (
+          skycons.responseJson.currently.icon("clear-night") === "clear-night"
+        ) {
+          this.setState({ [value]: Skycons.CLEAR_NIGHT });
+        } else if (
+          skycons.responseJson.currently.icon("clear-day") === "clear-day"
+        ) {
+          this.setState({ [value]: Skycons.CLEAR_DAY });
+        } else if (
+          skycons.responseJson.currently.icon("partly-cloudy-day") ===
+          "partly-cloudy-day"
+        ) {
+          this.setState({ [value]: Skycons.PARTLY_CLOUDY_DAY });
+        } else if (skycons.responseJson.currently.icon("rain") === "RAIN") {
+          this.setState({ [value]: Skycons.RAIN });
+        } else if (skycons.responseJson.currently.icon("cloudy") === "CLOUDY") {
+          this.setState({ [value]: Skycons.CLOUDY });
+        } else {
+          console.log("error!");
+
+          // skycons.add(document.getElementById("icon1"), Skycons.PARTLY_CLOUDY_DAY);
+          // skycons.add(document.getElementById("icon2"), Skycons.CLEAR_NIGHT);
+          // skycons.add(document.getElementById("icon3"), Skycons.CLEAR_DAY);
+          // skycons.add(document.getElementById("icon4"), Skycons.RAIN);
+          // skycons.add(document.getElementById("icon5"), Skycons.CLOUDY);
+        }
+        this.setState({
+          [value2]: responseJson.currently.temperature
+        });
       });
     } catch (e) {
       console.log("error", e);
@@ -99,7 +152,7 @@ class PopularWeather extends React.Component {
         <div className="row">
           <div className="column">
             <div className="card">
-              <canvas id="icon1" width="128" height="128" />
+              <canvas id="icon1" ref={this.ref1} width="128" height="128" />
               <h3>{this.state.weather1}</h3>
               <p>{this.state.country1}</p>
               <p>{this.state.temp1} celsius</p>
@@ -109,6 +162,7 @@ class PopularWeather extends React.Component {
 
           <div className="column">
             <div className="card">
+              <canvas id="icon2" ref={this.ref2} width="50" height="50" />
               <h3>{this.state.weather2}</h3>
               <p>{this.state.country2}</p>
               <p>{this.state.temp2} celsius</p>
@@ -118,6 +172,7 @@ class PopularWeather extends React.Component {
 
           <div className="column">
             <div className="card">
+              <canvas id="icon3" ref={this.ref3} width="50" height="50" />
               <h3>{this.state.weather3}</h3>
               <p>{this.state.country3}</p>
               <p>{this.state.temp3} celsius</p>
@@ -127,6 +182,7 @@ class PopularWeather extends React.Component {
 
           <div className="column">
             <div className="card">
+              <canvas id="icon4" ref={this.ref4} width="50" height="50" />
               <h3>{this.state.weather4}</h3>
               <p>{this.state.country4}</p>
               <p>{this.state.temp4} celsius</p>
@@ -136,6 +192,7 @@ class PopularWeather extends React.Component {
 
           <div className="column">
             <div className="card">
+              <canvas id="icon5" ref={this.ref5} width="50" height="50" />
               <h3>{this.state.weather5}</h3>
               <p>{this.state.country5}</p>
               <p>{this.state.temp5} celsius</p>
@@ -145,6 +202,49 @@ class PopularWeather extends React.Component {
         </div>
       </div>
     );
+  }
+  componentDidUpdate() {
+    const skycons = new Skycons({ color: "blue" });
+    // skycons.add(this.ref1.current, Skycons.PARTLY_CLOUDY_DAY);
+    // skycons.add(this.ref2.current, Skycons.CLEAR_NIGHT);
+    // skycons.add(this.ref3.current, Skycons.CLOUDY);
+    // skycons.add(this.ref4.current, Skycons.CLEAR_DAY);
+    // skycons.add(this.ref5.current, Skycons.RAIN);
+    let weather1 = this.state.weather1;
+    skycons.add(document.getElementById("icon1"), weather1);
+    let weather2 = this.state.weather2;
+    skycons.add(document.getElementById("icon2"), weather2);
+    let weather3 = this.state.weather3;
+    skycons.add(document.getElementById("icon3"), weather3);
+    let weather4 = this.state.weather4;
+    skycons.add(document.getElementById("icon4"), weather4);
+    let weather5 = this.state.weather5;
+    skycons.add(document.getElementById("icon5"), weather5);
+    //    skycons.add(document.getElementById("icon2"), Skycons.CLEAR_NIGHT);
+    //    skycons.add(document.getElementById("icon3"),
+    //    skycons.add(document.getElementById("icon4"), Skycons.RAIN);
+    //    skycons.add(document.getElementById("icon5"), Skycons.CLOUDY);
+    skycons.play();
+
+    // if (!skycons.responseJson.currently.icon) {
+    //     return (<h2>Loading...</h2>);
+    // }
+
+    // if(skycons.responseJson.currently.icon === 'wind') {
+    //     skycons.set("wind", Skycons.WIND);
+    //     skycons.play();
+    // } else if(skycons.responseJson.currently.icon === 'clear-day') {
+    //     skycons.set("clear-day", Skycons.CLEAR_DAY);
+    //     skycons.play();
+    // } else if(skycons.responseJson.currently.icon === 'partly-cloudy-day') {
+    //     skycons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+    //     skycons.play();
+    // } else if(skycons.responseJson.currently.icon === 'partly-cloudy-night') {
+    //     skycons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+    //     skycons.play();
+    // } else {
+    //     console.log('error!');
+    // }
   }
 }
 
