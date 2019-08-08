c3dc5d3b73f0863c3c7d5997b7f31f9d7c6dd2ef
Nasim Ali <nasim133@icloud.com>
2019-08-08 09:43:42 +0100

    Changed api key for weather and changed up flights

--- front-end/src/Components/TopFiveFlights.jsx
+++ front-end/src/Components/TopFiveFlights.jsx
@@ -47,12 +47,15 @@ class TopFiveFlights extends React.Component {
 
 
         this.getTop5Flights("1","DXB");
+        this.getTop5Flights("2","SYD");
+        this.getTop5Flights("3","SIN");
+        this.getTop5Flights("4","HND");
+        this.getTop5Flights("5","EDI");
 
     }
 
-    getInitialState() {
-        return { ["showModal"]: false };
-    }
+
+
 
     close() {
         this.setState({ ["showModal1"]: false });
@@ -132,21 +135,13 @@ class TopFiveFlights extends React.Component {
         return (
             <div>
 
-                {/*<Button*/}
-                {/*    bsStyle="primary"*/}
-                {/*    bsSize="large"*/}
-                {/*    onClick={this.open}*/}
-                {/*>*/}
-                {/*    Launch demo modal*/}
-                {/*</Button>*/}
-
                 <div className="row">
                     <div className="span2 offset1">
                         <div onClick={this.open}>
                             flight1
                         </div>
 
-                        <Modal show={this.state.showModal} onHide={this.close}>
+                        <Modal show={this.state.showModal1} onHide={this.close}>
                             <Modal.Header closeButton>
                                 <Modal.Title>London to Dubai</Modal.Title>
                             </Modal.Header>
@@ -194,14 +189,14 @@ class TopFiveFlights extends React.Component {
                                 <Modal.Title>London to Dubai</Modal.Title>
                             </Modal.Header>
                             <Modal.Body>
-                                <p> {this.state.flight1}</p>
-                                <p> {this.state.price1}</p>
-                                <p> {this.state.extra1}</p>
+                                <p> {this.state.flight2}</p>
+                                <p> {this.state.price2}</p>
+                                <p> {this.state.extra2}</p>
                             </Modal.Body>
                             <Modal.Footer>
                                 <div className="paypalButton">
                                     <PayPalButton
-                                        amount= {this.state.price1}
+                                        amount= {this.state.price2}
                                         onSuccess={(details, data) => {
                                             alert("Transaction completed by " + details.payer.name.given_name);
 
@@ -227,21 +222,123 @@ class TopFiveFlights extends React.Component {
 
                     </div>
                     <div className="span2">
-                        <div onClick={this.open}>
-                            flight1
+                        <div onClick={this.open2}>
+                            flight3
                         </div>
+
+                        <Modal show={this.state.showModal3} onHide={this.close2}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>London to Dubai</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <p> {this.state.flight3}</p>
+                                <p> {this.state.price3}</p>
+                                <p> {this.state.extra3}</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <div className="paypalButton">
+                                    <PayPalButton
+                                        amount= {this.state.price3}
+                                        onSuccess={(details, data) => {
+                                            alert("Transaction completed by " + details.payer.name.given_name);
+
+                                            // OPTIONAL: Call your server to save the transaction
+                                            return fetch("/paypal-transaction-complete", {
+                                                method: "post",
+                                                body: JSON.stringify({
+                                                    orderID: data.orderID
+                                                })
+                                            });
+                                        }}
+                                        options={{
+                                            clientId: "sb",
+                                            currency: "GBP"
+                                        }}
+                                    />
+                                </div>
+
+                            </Modal.Footer>
+                        </Modal>
+
                     </div>
                     <div className="span2">
-                        <div onClick={this.open}>
-                            flight1
+                        <div onClick={this.open3}>
+                            flight4
                         </div>
+
+                        <Modal show={this.state.showModal1} onHide={this.close3}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>London to Dubai</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <p> {this.state.flight4}</p>
+                                <p> {this.state.price4}</p>
+                                <p> {this.state.extra4}</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <div className="paypalButton">
+                                    <PayPalButton
+                                        amount= {this.state.price4}
+                                        onSuccess={(details, data) => {
+                                            alert("Transaction completed by " + details.payer.name.given_name);
+
+                                            // OPTIONAL: Call your server to save the transaction
+                                            return fetch("/paypal-transaction-complete", {
+                                                method: "post",
+                                                body: JSON.stringify({
+                                                    orderID: data.orderID
+                                                })
+                                            });
+                                        }}
+                                        options={{
+                                            clientId: "sb",
+                                            currency: "GBP"
+                                        }}
+                                    />
+                                </div>
+
+                            </Modal.Footer>
+                        </Modal>
 
                     </div>
                     <div className="span2">
 
-                        <div onClick={this.open}>
-                            flight1
+                        <div onClick={this.open4}>
+                            flight5
                         </div>
+                        <Modal show={this.state.showModal1} onHide={this.close4}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>London to Dubai</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <p> {this.state.flight5}</p>
+                                <p> {this.state.price5}</p>
+                                <p> {this.state.extra5}</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <div className="paypalButton">
+                                    <PayPalButton
+                                        amount= {this.state.price5}
+                                        onSuccess={(details, data) => {
+                                            alert("Transaction completed by " + details.payer.name.given_name);
+
+                                            // OPTIONAL: Call your server to save the transaction
+                                            return fetch("/paypal-transaction-complete", {
+                                                method: "post",
+                                                body: JSON.stringify({
+                                                    orderID: data.orderID
+                                                })
+                                            });
+                                        }}
+                                        options={{
+                                            clientId: "sb",
+                                            currency: "GBP"
+                                        }}
+                                    />
+                                </div>
+
+                            </Modal.Footer>
+                        </Modal>
 
                     </div>
                 </div>
